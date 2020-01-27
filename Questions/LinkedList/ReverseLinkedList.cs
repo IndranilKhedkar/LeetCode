c4677f4fadbbe7f9eb264d02cad42e8838054406
Indranil Khedkar <Indranil_Khedkar@symantec.com>
2020-01-27 18:21:51 +0530

    Tree: PathSum.cs

---
+++ Questions/LinkedList/ReverseLinkedList.cs
@@ -0,0 +1,44 @@
+﻿using Algorithms.Contracts;
+using Algorithms.Helpers.Attibutes;
+using Algorithms.Helpers.Enums;
+using LinkedList.Models;
+using System;
+
+namespace LinkedList
+{
+    /// <summary>
+    /// Referenc Link: 
+    /// </summary>
+    [RelatedTopic(Topic.LinkedList)]
+    public class ReverseLinkedList : Question
+    {
+        #region Interative Approach
+
+        public ListNode ReverseListInterative(ListNode head)
+        {
+            if (head == null || head.Next == null)
+                return head;
+
+            ListNode currNode = head.Next;
+            ListNode prevNode = head;
+            head.Next = null;
+
+            while (currNode != null)
+            {
+                ListNode temp = currNode.Next;
+                currNode.Next = prevNode;
+                prevNode = currNode;
+                currNode = temp;
+            }
+
+            return prevNode;
+        }
+
+        #endregion Interative Approach
+
+        public override void Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Questions/Tree/PathSum.cs
@@ -0,0 +1,38 @@
+﻿using Algorithms.Contracts;
+using Algorithms.Helpers.Attibutes;
+using Algorithms.Helpers.Enums;
+using Tree.Models;
+
+namespace Tree
+{
+    /// <summary>
+    /// Reference Link: https://leetcode.com/problems/path-sum/
+    /// </summary>
+    [RelatedTopic(Topic.Tree)]
+    [RelatedTopic(Topic.DFS)]
+    public class PathSum : Question
+    {
+        public bool HasPathSum(TreeNode root, int sum)
+        {
+            return HasPathSumDFS(root, 0, sum);
+        }
+
+        public override void Run()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private bool HasPathSumDFS(TreeNode node, int sum, int target)
+        {
+            if (node == null)
+                return false;
+
+            sum = sum + node.val;
+            if (node.left == null && node.right == null && target == sum)
+                return true;
+
+            return HasPathSumDFS(node.left, sum, target) || HasPathSumDFS(node.right, sum, target);
+        }
+
+    }
+}
