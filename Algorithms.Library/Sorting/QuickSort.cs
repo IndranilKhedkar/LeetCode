da7538cd7d7daa3154e4c9cf6f765cb8038a9c66
Indranil Khedkar <Indranil_Khedkar@symantec.com>
2020-03-29 22:21:24 +0530

    QuickSort.cs

---
+++ Algorithms.Library/Sorting/QuickSort.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms.Library.Sorting
+{
+    public class QuickSort<T>
+    {
+        public T[] Sort(T[] arr)
+        {
+            int length = arr.Length;
+            if (length <= 1)
+            {
+                return arr;
+            }
+
+            QuickSortRecursiveHelper(ref arr, 0, length - 1);
+
+            return arr;
+        }
+
+        private void QuickSortRecursiveHelper(ref T[] arr, int low, int high)
+        {
+            if (low < high)
+            {
+                int pivot = Partition(ref arr, low, high);
+                QuickSortRecursiveHelper(ref arr, low, pivot - 1);
+                QuickSortRecursiveHelper(ref arr, pivot + 1, high);
+            }
+        }
+
+        private int Partition(ref T[] arr, int low, int high)
+        {
+            T pivot = arr[high];
+            int i = (low - 1);
+
+            for (int j = low; j < high; j++)
+            {
+                if (Comparer<T>.Default.Compare(arr[j], pivot) <= 0)
+                {
+                    if (++i != j)
+                    {
+                        Swap(ref arr[i], ref arr[j]);
+                    }
+                }
+            }
+
+            Swap(ref arr[i + 1], ref arr[high]);
+
+            return i + 1;
+        }
+
+        private void Swap(ref T item1, ref T item2)
+        {
+            T temp = item1;
+            item1 = item2;
+            item2 = temp;
+        }
+    }
+}
---
+++ UnitTests/Algorithms.Library.Tests/Sorting/QuickSortTests.cs
@@ -0,0 +1,60 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithms.Library.Sorting.Tests
+{
+    [TestClass]
+    public class QuickSortTests
+    {
+        [TestMethod]
+        public void Sort_For_IntArrayInDescendingOrder_Returns_SortedArrayInAscendingOrder()
+        {
+            /* Arrange */
+            int[] A = new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+
+            /* Act */
+            var result = new QuickSort<int>().Sort(A);
+
+            /* Assert */
+            Assert.IsNotNull(A);
+            for (int i = 0; i < A.Length; i++)
+            {
+                Assert.AreEqual(i, A[i]);
+            }
+        }
+
+        [TestMethod]
+        public void Sort_For_IntArrayInAscendingOrder_Returns_SortedArrayInAscendingOrder()
+        {
+            /* Arrange */
+            int[] A = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            /* Act */
+            var result = new QuickSort<int>().Sort(A);
+
+            /* Assert */
+            Assert.IsNotNull(A);
+            for (int i = 0; i < A.Length; i++)
+            {
+                Assert.AreEqual(i, A[i]);
+            }
+        }
+
+        [TestMethod]
+        public void Sort_For_IntArray_Returns_SortedArrayInAscendingOrder()
+        {
+            /* Arrange */
+            int[] A = new int[] { 3, 5, 1, 3, 9, 0, 5612, 123, 57, 9, 123 };
+            int[] expectedResult = new int[] { 0, 1, 3, 3, 5, 9, 9, 57, 123, 123, 5612 };
+
+            /* Act */
+            var result = new QuickSort<int>().Sort(A);
+
+            /* Assert */
+            Assert.IsNotNull(A);
+            for (int i = 0; i < A.Length; i++)
+            {
+                Assert.AreEqual(expectedResult[i], A[i]);
+            }
+        }
+    }
+}
